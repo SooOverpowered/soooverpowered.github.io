42676e24e3ee27a355018327dfc87f71a78d9fb8
SooOverpowered <lehoangminh15122000@gmail.com>
2021-07-01 19:57:08 +0700

    commit

--- sw.js
+++ sw.js
@@ -2,18 +2,19 @@
 // only change from here down
 const CACHE_NAME = "my-site-v1";
 const CACHE_URLS = [
-	"../index.html",
-	"../ShopMenu.html",
-	"../TableBooking.html",
-	"../AboutShop.html",
-	"../AboutCoffee.html",
-	"../manifest.json",
-	"../css/main.css",
-	"../css/shopmenu.css",
-	"../404.html",
-	"../img/",
-	"../fonts/CountrysideTwo.ttf",
-    "../fonts/MYRIADPRO-REGULAR.OTF"
+	"/",
+	"/index.html",
+	"/ShopMenu.html",
+	"/TableBooking.html",
+	"/AboutShop.html",
+	"/AboutCoffee.html",
+	"/manifest.json",
+	"/css/main.css",
+	"/css/shopmenu.css",
+	"/404.html",
+	"/img/",
+	"/fonts/CountrysideTwo.ttf",
+    "/fonts/MYRIADPRO-REGULAR.OTF"
 ];
 
 //DO NOT change any of the code below
